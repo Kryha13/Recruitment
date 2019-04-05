b74e6ac1b6bf6c15de3a7e0356d4c299bfc3c6c2
Krystian Baranowski <krystian.qaz123@gmail.com>
2019-04-05 15:55:53 +0200

    change recruiter concept

--- recruitment/models.py
+++ recruitment/models.py
@@ -1,13 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-
-class Recruiter(models.Model):
-    first_name = models.TextField(max_length=50)
-    last_name = models.TextField(max_length=50)
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
 
 
 class Task(models.Model):
@@ -29,7 +23,8 @@ class Grade(models.Model):
     value = models.DecimalField(max_digits=2, decimal_places=1)
     task = models.OneToOneField(Task, on_delete=models.CASCADE)
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
-    recruiter = models.ForeignKey(Recruiter, on_delete=models.PROTECT)
+    recruiter = models.ForeignKey(User, on_delete=models.PROTECT)
 
     def __str__(self):
         return f'{self.candidate} - {self.value}'
+
